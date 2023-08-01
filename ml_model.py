16cb7241b545beb4aad027485f123a75c37b0351
kukki.yogi@gmail.com <kukki.yogi@gmail.com>
2023-08-01 18:37:23 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("adding allk ml related code here")
+
+def model1():
+    print("adding code for ML Model1")
+
+def model2():
+    print("adding code for ML Model2")
+
+def model2():
+    print("adding code for ML Model2")
